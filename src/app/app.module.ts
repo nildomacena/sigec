84f36fedfd226713745ca94aec6eabb0d2fb53e6
Ednildo Macena <nildo.777@gmail.com>
2023-05-22 11:58:38 -0300

    first cmmit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,47 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AppBarComponent } from './components/app-bar/app-bar.component';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { HomeComponent } from './pages/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenubarModule } from 'primeng/menubar';
+import { CadastrarEstruturaComponent } from './pages/estruturas/cadastrar-estrutura/cadastrar-estrutura.component';
+import { DetalharEstruturaComponent } from './pages/estruturas/detalhar-estrutura/detalhar-estrutura.component';
+import { ListarEstruturasComponent } from './pages/estruturas/listar-estruturas/listar-estruturas.component';
+import { TableModule } from 'primeng/table';
+import { TabViewModule } from 'primeng/tabview';
+import { InputTextModule } from 'primeng/inputtext';
+import { NgxMaskDirective } from 'ngx-mask'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AppBarComponent,
+    HomeComponent,
+    CadastrarEstruturaComponent,
+    DetalharEstruturaComponent,
+    ListarEstruturasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ToolbarModule,
+    ButtonModule,
+    SplitButtonModule,
+    BrowserAnimationsModule,
+    TableModule,
+    MenubarModule,
+    TabViewModule,
+    InputTextModule,
   ],
-  providers: [],
+  providers: [
+    provideEnvironmentNgxMask(),
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
